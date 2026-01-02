7fd120c00c81439587f29f969c6d02ad10d3c9f7
Akash Sharma <aakasharma0900@gmail.com>
2026-01-02 15:59:13 +0530

    FunctionalPrograming commit2

--- FunctionalPrograming/FunctionDemo/FunctionDemo.java
+++ FunctionalPrograming/FunctionDemo/FunctionDemo.java
@@ -1,11 +1,21 @@
 package FunctionalPrograming.FunctionDemo;
 
+import java.util.concurrent.Future;
 import java.util.function.Function;
 
 public class FunctionDemo {
-    
+
     public static void main(String[] args) {
-        Function<Integer,Integer> square =x ->x*x;
-        System.out.println("Square of 5 is: "+square.apply(5));
+        Function<Integer, Integer> square = x -> x * x;
+        System.out.println("Square of 5 is: " + square.apply(5));
+        Function<Integer, Integer> increment = x -> x + 1;
+
+        // Chaninging functions
+        Function<Integer, Integer> squareThenIncrement = square.andThen(increment);
+        Function<Integer, Integer> squareThenIncrement2 = increment.compose(square);
+
+        Function<Integer, Integer> doubleIt = x -> x * 2;
+        System.out.println(square.andThen(increment).apply(5)); // first square then increment
+
     }
 }
